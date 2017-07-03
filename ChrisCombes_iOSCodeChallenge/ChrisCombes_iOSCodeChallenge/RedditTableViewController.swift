a49da571f37b9e2844b45cc10d83869c331acc53
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2017-07-03 13:36:10 -0500

    Running initial search query.

--- ChrisCombes_iOSCodeChallenge/ChrisCombes_iOSCodeChallenge/RedditTableViewController.swift
+++ ChrisCombes_iOSCodeChallenge/ChrisCombes_iOSCodeChallenge/RedditTableViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class RedditTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class RedditTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    private var queryResult:RedditQuery?
     
     static func redditTableViewController() ->RedditTableViewController {
         let controller = RedditTableViewController(nibName: "RedditTableView", bundle: nil)
@@ -18,10 +22,45 @@ class RedditTableViewController: UIViewController, UITableViewDataSource, UITabl
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
-
+    
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         cell.textLabel?.text = String(format: "%d", indexPath.row)
         return cell
+    }
+    
+    // MARK: Helpers
+    func runSearchWithText(_ text: String) {
+        
+        // Stop further queries until current result completes
+        searchBar.isUserInteractionEnabled = false
+        
+        RedditAPIManager().postsForCategory(text) { (json) in
+            if json != JSON.null {
+                if let result = RedditQuery(json: json) {
+                    self.queryResult = result
+                }
+            }
+            
+            DispatchQueue.main.sync {
+                // Allow search again
+                self.searchBar.isUserInteractionEnabled = true
+            }
+        }
+    }
+    
+    // MARK: Actions
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+        if let searchText = searchBar.text {
+            runSearchWithText(searchText)
+        }
+    }
+    
+    // MARK: UISearchBarDelegate
+    public func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        if let searchText = searchBar.text {
+            runSearchWithText(searchText)
+        }
     }
 }
