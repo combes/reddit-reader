a81d80fff83f0d8b88bd0b62c5b27527dd6b82bf
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2017-07-02 23:27:11 -0500

    Added image assets, fonts, and NetworkHelper class.

---
+++ ChrisCombes_iOSCodeChallenge/ChrisCombes_iOSCodeChallenge/NetworkHelper.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkHelper.swift
+//  ChrisCombes_iOSCodeChallenge
+//
+//  Created by Christopher Combes on 7/2/17.
+//  Copyright © 2017 Christopher Combes. All rights reserved.
+//
+
+import SystemConfiguration
+
+// Found a handy helper
+// https://stackoverflow.com/questions/38726100/best-approach-for-checking-internet-connection-in-ios
+func isInternetAvailable() -> Bool {
+    
+    var zeroAddress = sockaddr_in()
+    zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+    zeroAddress.sin_family = sa_family_t(AF_INET)
+    
+    guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+        $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+            SCNetworkReachabilityCreateWithAddress(nil, $0)
+        }
+    }) else {
+        return false
+    }
+    
+    var flags: SCNetworkReachabilityFlags = []
+    if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+        return false
+    }
+    
+    let isReachable = flags.contains(.reachable)
+    let needsConnection = flags.contains(.connectionRequired)
+    
+    return (isReachable && !needsConnection)
+}
+
